6376824e06799f11ceb984259ad80477781b6397
Jan Vabek <vabekjan@gmail.com>
2020-06-27 17:54:37 +0200

    The code now requires specify the input arguments and CUPRAD inputs are in separate branch (we probably merge later). The script now append new group with parameters into an existing archive. There is source and target archives now, the target is the source with the appended inputs. We now use only free-form.

--- create_universal_HDF5.py
+++ create_universal_HDF5.py
@@ -15,7 +15,8 @@
 import numpy as np
 import h5py
 import sys
-import os.path
+import os
+import shutil
 
 #### THE MAIN PROGRAM #####
 
@@ -33,58 +34,34 @@ def verify_input_file(filename):
        print("Error: File {file} does not exists.".format(file=inputfilename))
        exit(1)
 
-
+## dealing with the input arguments
 output_file = ""
 arguments = sys.argv
-if len(arguments) == 1:
-    inputfilename = "FreeFormInputs.inp"
-    print("Using default filename:", inputfilename)
-elif len(arguments) > 4:
-    print('Too many arguments.\nNumber of arguments:', len(arguments))
-    exit(1)
-elif len(arguments) == 4:
-    print("Trying to locate output file.")
-    if not "-o" in arguments[1:3]:
-        print("Wrong arguments.\n")
-        help()
-    else:
-        o_index = arguments.index("-o")
-        o_f_index = o_index+1
-        output_file = arguments[o_f_index]
-        if os.path.exists(output_file):
-            print("file exists")
-            answered = False
-            while not answered:
-                confirmation = input("Do you really want to overwrite an existing file {o_f}? [Y/N]".format(o_f = output_file))
-                if confirmation in "yY":
-                    answered = True
-                elif confirmation in "nN":
-                    answered = True
-                    output_file = ""
-                    print("Output file not selected. It will not be created.")
-        if o_index == 1:
-            inputfilename = arguments[3]
-        else:
-            inputfilename = arguments[1]
-        verify_input_file(inputfilename)
-elif len(arguments) == 3:
-    print("You are either missing an argument or you have one extra.\n")
-    help()
-    exit(1)
+# print(arguments)
+if len(arguments) == 9: # the input are -i [input file] -ihdf5 [input archive] -ohdf5 [output archive] -g [the group with inputs]
+    arg_index = arguments.index("-i")
+    inputfilename = arguments[arg_index+1]
+    arg_index = arguments.index("-ihdf5")
+    source_archive = arguments[arg_index+1]
+    arg_index = arguments.index("-ohdf5")
+    target_archive = arguments[arg_index+1]
+    arg_index = arguments.index("-g")
+    groupname = arguments[arg_index+1]
 else:
-    if arguments[1] == "--help" or arguments[1] == "-h":
-        help()
-    else:
-        inputfilename = arguments[1]
-        verify_input_file(inputfilename)
+    print("The code now requires precise specification of input arguments")
+    exit(1)
+
+
 
 # inputfilename = 'FreeFormInputs.inp'
-outputfilename = 'results.h5'
+# outputfilename = 'results.h5'
 
 ## specify the name of files and datasets
+
+shutil.copyfile(source_archive,target_archive)
 InputFile = open(inputfilename, "r")
-GeneratedFile = h5py.File(outputfilename, 'w')  # be careful, now it trucates if already exists
-grp = GeneratedFile.create_group('inputs')
+GeneratedFile = h5py.File(target_archive, 'a')  # access option http://docs.h5py.org/en/stable/high/file.html#file
+grp = GeneratedFile.create_group(groupname)
 
 
 def add_dataset(h_path, sep_line, line):
@@ -134,83 +111,11 @@ def add_dataset_array(h_path, sep_line, line):
     dset_id.attrs['units'] = np.string_('[' + unit + ']')
 
 
-def format_to_freeform(line):
-    line = line.strip()
-    if bool(line) and not line[0] in "[#":
-
-        # Split the line and find name of the variable with the value
-        line = line.lower().split(":")
-        if len(line) >= 2:
-            if len(line) > 2:
-                name = ":".join(line[:-1])
-            else:
-                name = line[0].strip()
-            value = line[-1].strip()
-
-            # Replace all the brackets with square brackets
-            brackets = {"(": "[", ")": "]"}
-            for b in brackets:
-                name = name.replace(b, brackets[b])
-    
-            # Try to locate units
-            unit = ""
-            if "[" in name:
-                s = name.rfind("[")
-                unit = name[s:]
-                name = name[:s]
-            for b in brackets:
-                name = name.replace(brackets[b], b)
-
-            # Try to find the set of numbers where the value belongs
-            number_set = "I"
-            if len(value) < 1:
-                return ""
-            if value[0].isalpha() or "_" in value:
-                number_set = "S"
-            elif (value[0].isnumeric() or (value[0] == "-" and value[1].isnumeric())) and "d" in value:
-                number_set = "R"
-                if ".d" in value:
-                    value = value.replace("d", "0e")
-                else:
-                    value = value.replace("d", "e")
-
-            # Reformat the name of the variable
-            name = name.replace(",", " ")
-            name = name.replace(" - ", "-")
-            name = name.replace("/", " ")
-            name = "_".join(name.split())
-            line = " ".join([name, value, number_set, unit])
-            
-            return line
-        else:
-            return ""
-    else:
-        return ""
 
 
 lines = InputFile.readlines()
-variables = []
-first = True
-formatted = True
-out = False
-
-if not output_file == "":
-    out = True
-    output = open(output_file, "w")
-    output.write("[FreeFormFormat]\n")
 
 for line in lines:
-    if first:
-        first = False
-        if not "[FreeFormFormat]" in line:
-            print("File not formatted.")
-            formatted = False
-    if not formatted:
-        if not type(line) == type("s"):
-            continue
-        line = format_to_freeform(line)
-        if out:
-            output.write(line+"\n")
     sep_line = line.split();  # separate the line
     if ((len(sep_line) == 0) or (sep_line[0] == '#') or (sep_line[0] == '##')):
         pass  # print('empty or commented line')
@@ -218,8 +123,7 @@ for line in lines:
         add_dataset_array(grp, sep_line, line)  # at the moment we need precise alignment
     else:
         add_dataset(grp, sep_line, line)
-if out:
-    output.close()
+
 
 InputFile.close()
 GeneratedFile.close()
