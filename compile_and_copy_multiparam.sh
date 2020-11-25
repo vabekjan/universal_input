8f4ad5d6eee5333f1db87b643410e6c896bc081d
Jan Vabek <vabekjan@gmail.com>
2020-11-25 16:43:20 +0100

    compilation script shortened

--- compile_and_copy_multiparam.sh
+++ compile_and_copy_multiparam.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-# First testing procedure
-
-rm params/*
-
 gfortran all_combinations/libraries.f90 all_combinations/main.f90 -o all_combinations.e
-
-# ./all_combinations.e
-
-echo "done"
+echo "compiled"
 
