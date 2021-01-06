3d9157ef046bba1fe85b01d6fcc29f7eaeb1d556
Jan Vabek <vabekjan@gmail.com>
2021-01-06 23:20:18 +0100

    attempt to include groups in the multiparamtric script

--- process_multiparametric.py
+++ process_multiparametric.py
@@ -26,6 +26,7 @@ else:
     arg_index = arguments.index("-univ-inps")
     run_args = run_args + arguments[(arg_index+1):]
     keep_intermadiate = ("-keep-intermediate" in arguments)
+    grouped = ("-multiparam-groups" in arguments)
 
 
 ## Prepare files for the FORTRAN code
@@ -34,6 +35,11 @@ with open(inputfilenamemp, "r") as InputMP:
 names = ''
 dtypes = ''
 units = ''
+groups = ''
+if grouped:
+    shift = 1
+else:
+    shift = 0
 firstline = True
 n_params = 0
 for line in lines:
@@ -43,13 +49,14 @@ for line in lines:
     else:
         n_params = n_params + 1
         names = names + sep_line[0] + '\t'
-        dtypes = dtypes + sep_line[1] + '\t'
-        units = units + sep_line[2] + '\t'
+        if grouped: groups = groups + sep_line[1] + '\t'
+        dtypes = dtypes + sep_line[shift+1] + '\t'
+        units = units + sep_line[shift+2] + '\t'
         if firstline:
-            content = sep_line[3] + '\t' + sep_line[4] + '\t' + sep_line[5]
+            content = sep_line[shift+3] + '\t' + sep_line[shift+4] + '\t' + sep_line[shift+5]
             firstline = False
         else:
-            content = content + '\n' + sep_line[3] + '\t' + sep_line[4] + '\t' + sep_line[5]
+            content = content + '\n' + sep_line[shift+3] + '\t' + sep_line[shift+4] + '\t' + sep_line[shift+5]
 
 with open('multiparam_FORTRAN.inp','w') as f:
     f.write(content)
@@ -65,7 +72,9 @@ with open('list_of_combinations_FORTRAN.dat') as f:
     content = f.read()
 if (not keep_intermadiate): os.remove('list_of_combinations_FORTRAN.dat')
 
-content = '$multiparametric\t' + str(content.count('\n')) + '\n' + names + '\n' + dtypes + '\n' + units + '\n' + content
+content = '$multiparametric\t' + str(content.count('\n')) + '\n' + names + '\n'
+if grouped: content = content + groups + '\n'
+content = content + dtypes + '\n' + units + '\n' + content
 
 with open(intermediate_filename,'w') as f_tmp, open(inputfilename,'r') as f_reg:
     f_tmp.write(f_reg.read()+'\n'+content)
