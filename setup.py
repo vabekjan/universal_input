15af2255f060570bd931bfa692695724c51095ca
Jan Vabek <vabekjan@gmail.com>
2024-10-16 17:32:01 +0200

    modify the setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package_name',
+    name='universal_inputs',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'h5py',  # Specify version or just the package name
         'numpy'  
     ],
+    entry_points={
+        'console_scripts': [
+            'create_universal_HDF5=create_universal_HDF5',  # The command `my_script` calls the script directly
+        ],
+    },
 )
